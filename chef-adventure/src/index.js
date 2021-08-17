adc6c6be76bc505c83db055e794c3fba044a33ff
JE <jragyn@gmail.com>
2021-08-16 19:00:50 -0700

    star ocean what

--- chef-adventure/src/index.js
+++ chef-adventure/src/index.js
@@ -1,7 +1,8 @@
-import { Greeter } from './test';
-
-const g = new Greeter();
-g.memorize("secret words")
-Greeter.say("hello world");
-Greeter.say(123);
-g.repeat();
+import * as Core from './core/_main';
+import * as BattleManagerOverwrites from './managers/BattleManager';
+import * as DataManagerOverwrites from './managers/DataManager';
+import * as SceneMapOverwrites from './scenes/SceneMap';
+import * as GameInterpreterOverwrites from './objects/GameInterpreter';
+import * as GameMapOverwrites from './objects/GameMap';
+import * as GamePlayerOverwrites from './objects/GamePlayer';
+import * as GameTroopOverwrites from './objects/GameTroop';
