2d133e6723d87a15d72063c8338f751e409167ff
je <jragyn@gmail.com>
2023-08-17 15:33:04 -0700

    remove the source;

--- chef-adventure/js/src/j/abs/core/objects/Game_Follower.js
+++
@@ -1,8 +0,0 @@
-/**
- * Whether or not this character is a follower.
- * @returns {boolean} True if this is a follower, false otherwise.
- */
-Game_Follower.prototype.isFollower = function()
-{
-  return true;
-};
